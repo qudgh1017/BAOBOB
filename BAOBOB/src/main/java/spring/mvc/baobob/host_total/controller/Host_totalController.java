d3764dabbf845f1b4f9218373216c5884a3ea6e7
KO_03 <KO_03@KO_03-PC>
2018-01-08 19:19:45 +0900

    Log출력 환경설정완료.(Host_totalController)

--- BAOBOB/src/main/java/spring/mvc/baobob/host_total/controller/Host_totalController.java
+++ BAOBOB/src/main/java/spring/mvc/baobob/host_total/controller/Host_totalController.java
@@ -1,8 +1,29 @@
 package spring.mvc.baobob.host_total.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Host_totalController {
-
+	
+	private Logger log = Logger.getLogger(this.getClass());
+	
+	@RequestMapping("adminDefault")
+	public String adminDefault(Model model) {
+		
+		log.debug("====== host_total.controller/adminDefault ======");
+		
+		return "adminDefault";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
