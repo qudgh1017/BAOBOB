2ed218712306a15c57595c6a4c3a2a626fbdbfbd
gjrjr <gjrjr@DESKTOP-D2IIFVE>
2018-01-10 13:13:25 +0900

    host_movie controller, persistence, service 수정 MovieVO 추가

---
+++ BAOBOB/src/main/java/spring/mvc/baobob/vo/MovieVO.java
@@ -0,0 +1,122 @@
+package spring.mvc.baobob.vo;
+
+public class MovieVO {
+	private int movie_index;
+	private String movie_title;
+	private String movie_content;
+	private int movie_janre;
+	private int movie_age;
+	private String movie_rel_date;
+	private String movie_director;
+	private String movie_star;
+	private String movie_country;
+	private int movie_runTime;
+	private String movie_poster;
+	private String movie_trailer;
+	private int movie_state;
+
+	public int getMovie_index() {
+		return movie_index;
+	}
+
+	public void setMovie_index(int movie_index) {
+		this.movie_index = movie_index;
+	}
+
+	public String getMovie_title() {
+		return movie_title;
+	}
+
+	public void setMovie_title(String movie_title) {
+		this.movie_title = movie_title;
+	}
+
+	public String getMovie_content() {
+		return movie_content;
+	}
+
+	public void setMovie_content(String movie_content) {
+		this.movie_content = movie_content;
+	}
+
+	public int getMovie_janre() {
+		return movie_janre;
+	}
+
+	public void setMovie_janre(int movie_janre) {
+		this.movie_janre = movie_janre;
+	}
+
+	public int getMovie_age() {
+		return movie_age;
+	}
+
+	public void setMovie_age(int movie_age) {
+		this.movie_age = movie_age;
+	}
+
+	public String getMovie_rel_date() {
+		return movie_rel_date;
+	}
+
+	public void setMovie_rel_date(String movie_rel_date) {
+		this.movie_rel_date = movie_rel_date;
+	}
+
+	public String getMovie_director() {
+		return movie_director;
+	}
+
+	public void setMovie_director(String movie_director) {
+		this.movie_director = movie_director;
+	}
+
+	public String getMovie_star() {
+		return movie_star;
+	}
+
+	public void setMovie_star(String movie_star) {
+		this.movie_star = movie_star;
+	}
+
+	public String getMovie_country() {
+		return movie_country;
+	}
+
+	public void setMovie_country(String movie_country) {
+		this.movie_country = movie_country;
+	}
+
+	public int getMovie_runTime() {
+		return movie_runTime;
+	}
+
+	public void setMovie_runTime(int movie_runTime) {
+		this.movie_runTime = movie_runTime;
+	}
+
+	public String getMovie_poster() {
+		return movie_poster;
+	}
+
+	public void setMovie_poster(String movie_poster) {
+		this.movie_poster = movie_poster;
+	}
+
+	public String getMovie_trailer() {
+		return movie_trailer;
+	}
+
+	public void setMovie_trailer(String movie_trailer) {
+		this.movie_trailer = movie_trailer;
+	}
+
+	public int getMovie_state() {
+		return movie_state;
+	}
+
+	public void setMovie_state(int movie_state) {
+		this.movie_state = movie_state;
+	}
+
+}
