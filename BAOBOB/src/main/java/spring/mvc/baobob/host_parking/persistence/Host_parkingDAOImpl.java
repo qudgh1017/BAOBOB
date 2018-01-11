765f52a715fda00ecfa36860b75f3b296129f6fd
kkk <kkk@kkk-PC>
2018-01-11 21:24:44 +0900

    실시간 주차 현황 작업

--- BAOBOB/src/main/java/spring/mvc/baobob/host_parking/persistence/Host_parkingDAOImpl.java
+++ BAOBOB/src/main/java/spring/mvc/baobob/host_parking/persistence/Host_parkingDAOImpl.java
@@ -1,10 +1,13 @@
 package spring.mvc.baobob.host_parking.persistence;
 
+import java.util.ArrayList;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import spring.mvc.baobob.vo.Parking;
+import spring.mvc.baobob.vo.ParkingFee;
 import spring.mvc.baobob.vo.ParkingSpace;
 
 @Repository
@@ -55,23 +58,73 @@ public class Host_parkingDAOImpl implements Host_parkingDAO {
 	@Override
 	public int parkingChange(Parking space) {
 		//해당 구역 정보 존재 여부
-		int idxChk = 0;
+		Parking p = getParking(space.getPark_index());
 
 		int cnt = 0;
-		if(idxChk != 0) {
+		if(p != null) {
+			space.setPark_last_date(p.getPark_last_date());
 			parkingUpdate(space);
+			cnt = 1;
 		} else {
 			Host_parkingDAO mapper = sqlSession.getMapper(Host_parkingDAO.class);
 			cnt = mapper.parkingChange(space);
 		}
-				
 		return cnt;
 	}
 	
 	//주차장 각 구역 정보 수정
 	@Override
 	public int parkingUpdate(Parking space) {
+		Host_parkingDAO mapper = sqlSession.getMapper(Host_parkingDAO.class);
+		int cnt = mapper.parkingUpdate(space);
+		return cnt;
+	}
+
+	//주차장 구역 상태 정보
+	@Override
+	public ArrayList<String> getParkingStates(int last_idx) {
+		Host_parkingDAO mapper = sqlSession.getMapper(Host_parkingDAO.class);
+		ArrayList<String> list = mapper.getParkingStates(last_idx);
+		return list;
+	}
+
+	//주차 기본 요금 설정 확인
+	@Override
+	public ParkingFee getParkingFee() {
+		Host_parkingDAO mapper = sqlSession.getMapper(Host_parkingDAO.class);
+		ParkingFee pf =  mapper.getParkingFee();
+		return pf;
+	}
+	
+	//주차 기본 요금 존재 여부
+	@Override
+	public int getParkingFeeCount() {
+		Host_parkingDAO mapper = sqlSession.getMapper(Host_parkingDAO.class);
+		int cnt = mapper.getParkingFeeCount();
+		return cnt;
+	}
+
+	//주차 기본 요금 등록
+	@Override
+	public int parkingFeeChange(ParkingFee pf) {
+		int count = getParkingFeeCount();
+		
 		int cnt = 0;
+		if(count == 0) {
+			Host_parkingDAO mapper = sqlSession.getMapper(Host_parkingDAO.class);
+			cnt = mapper.parkingFeeChange(pf);
+		} else {
+			parkingFeeUpdate(pf);
+			cnt = 1;
+		}
+		return cnt;
+	}
+
+	//주차 기본 요금 수정
+	@Override
+	public int parkingFeeUpdate(ParkingFee pf) {
+		Host_parkingDAO mapper = sqlSession.getMapper(Host_parkingDAO.class);
+		int cnt = mapper.parkingFeeUpdate(pf);
 		return cnt;
 	}
 	
--- BAOBOB/src/main/webapp/WEB-INF/views/host/host_parking/hostParkingState.jsp
+++ BAOBOB/src/main/webapp/WEB-INF/views/host/host_parking/hostParkingState.jsp
@@ -4,21 +4,34 @@
 <%@ include file="/resources/setting.jsp"%>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Insert title here</title>
+	
+	<style type="text/css">
+		body .p_body{min-height:450px;}
+		body .p_div{display:flex;width:fit-content;margin:0 auto;}/* flex-wrap:wrap; */
+		.p_img{width:100%;height:auto;}
+		body .p_btn{max-width:40px;height:fit-content;background:lightgray;border:transparent;border-radius:0.2em;padding:5px;margin:2px;} /* flex:1 0 120px; */
+	</style>
 </head>
-<body class="fixed-nav sticky-footer bg-dark" onload="parkingState(${pSpace.p_space_col}, ${pSpace.p_space_row});">
+<body class="fixed-nav sticky-footer bg-dark" onload="parkingStatus('${col}', '${row}', '${states}');">
 
 	<!-- Navigation -->
-	<%@ include file="../common/navigation.jsp" %>
+	<%@ include file="hostParkingNavigation.jsp" %>
 	
 	<div class="content-wrapper">
-	
-	
-	
+		<div class="container-fluid">
+			<div class="card mb-3">
+				<div class="card-header">실시간 주차 현황</div>
+				<div class="card-body p_body" id="spaceDiv"></div>
+				<div class="card-footer small text-muted">Update 2018-11-11 10:00</div>
+			</div>
+		</div>
 	</div>
 	
 	<!-- Footer -->
 	<%@ include file="../common/footer.jsp" %>
+	
+	<script src="${projectRes}ymk/js/parkingState.js"></script>
 </body>
 </html>
